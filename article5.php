28f9f06c3ce2b3e446e26836ebcee25e3dc158de
Andrea Ristori <andrea.ristori@gmail.com>
2013-12-29 15:39:21 +0100

    More article markup

---
+++ article5.php
@@ -0,0 +1,80 @@
+<html lang="en-US">
+    <?php include('header.php') ?>
+<div id="article-cover">
+	<div class="container">
+		<img class="img-responsive" src="http://placehold.it/1280x480">
+		<div class="row">
+		</div>
+	</div>
+</div>
+<div id="article-content">
+	<div class="container epsilon">
+		<div class="row">
+			<div id="headline" class="col-lg-12">
+				<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+					<h1 class="gamma">Hermes Collection</h1>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+				</div>
+			</div>
+			<div class="col-lg-8 col-md-8 col-sm-8">
+				<ol>
+<li>100 Contemporary Architects
+<em>Philip Jodidio</em></li>
+</ol>
+
+<h1>Keeping up with the fast-paced world of architecture</h1>
+
+<h2>The indispensable compendium of the most cutting-edge architects around</h2>
+
+<p>This two-volume compilation brings together highlights from TASCHEN’s groundbreaking title Building a New Millennium and the first four volumes of our renowned Architecture Now! books for a comprehensive study of architecture around the world at the dawn of the 21st century. Up-and-coming architects—tomorrow’s superstars—are featured alongside the field’s most respected practitioners, such as <em>* Santiago Calatrava, Herzog &amp; de Meuron, Koolhaas/OMA, Daniel Libeskind, Richard Meier, Oscar Niemeyer, and Jean Nouvel.</em>*</p>
+
+<h3>The author:</h3>
+
+<p>Philip Jodidio (born 1954) studied art history and economics at Harvard, and edited Connaissance des Arts for over 20 years. His books include TASCHEN’s Architecture Now! series, and monographs on Tadao Ando, Norman Foster, Richard Meier, Jean Nouvel, and Zaha Hadid. He is internationally renowned as one of the most popular writers on the subject of architecture.</p>
+
+<ol>
+<li>100 Illustrators
+<em>Steven Heller, Julius Wiedemann</em></li>
+</ol>
+
+<h1>The biggest players in the highly competitive field of illustration</h1>
+
+<h2>Culled from our Illustration Now! series is this selection of today’s most successful and important illustrators from around the globe. In his introduction, Steven Heller fleshes out the challenging process of narrowing down a field of 600 illustrators to a selection of the 100 most significant. The final cut, including artists such as Istvan Banyai, Gary Baseman, Seymour Chwast, Paul Davis, Brad Holland, Mirko Ilić, Anita Kunz, and Christoph Niemann, provides a snapshot of the highly dynamic and diverse world of contemporary illustration.</h2>
+
+<p><strong>Features include:</strong>
+* A self-portrait by each illustrator and examples of their work
+* A profile text about each illustrator by Steven Heller
+* Lists of selected exhibitions and publications
+* Illustrators’ quotes about his/her work</p>
+
+<h3>The editors:</h3>
+
+<p><strong>Steven Heller</strong> is the co-chair of the School of Visual Arts MFA Designer as Author Program. For 33 years he was an art director for The New York Times, and currently writes the "Visuals" column for The New York Times Book Review. He is the author of 120 books on graphic design, illustration, and satiric art.
+<strong>Julius Wiedemann</strong> was born in Brazil, studied graphic design and marketing, and was an art editor for digital and design magazines in Tokyo. His many TASCHEN digital and media titles include Illustration Now!, Advertising Now, Logo Design, and Brand Identity Now!</p>
+
+<ol>
+<li>Erotica Universalis  Gilles Néret</li>
+</ol>
+
+<h1>The art of pleasure</h1>
+
+<h2>An erotic history of humanity</h2>
+
+<p>From the dawn of time, ever since Adam and Eve, all artists of every age—whether the Egyptian, Greek, or Roman artists of Antiquity, or more recent famous names as Rembrandt, Courbet, Degas, or Picasso—have succumbed to their fantasies, obsessions, and libido, and produced erotic works that the censors have taken good care to keep from the public. For Erotica Universalis, we surface from the subterranean realms of the museums to enter those of our national and private libraries. Here we discover that not only most of our famous writers, such as Ovid, Aretino, Voltaire, Verlaine, or Maupassant, wrote erotic texts that bordered on indecency, but also that great artists like Boucher, Fragonard, Dalí, or Matisse were inspired to provide suitable illustrations for these naughty books. For this new hardcover edition of the classic 1995 bestseller, we have culled highlights from our Erotica Universalis collection. A banquet for the imagination and a feast of the eye! </p>
+
+<h3>The author:</h3>
+
+<p>Gilles Néret (1933–2005) was an art historian, journalist, writer and museum correspondent. He organized several art retrospectives in Japan and founded the SEIBU museum and the Wildenstein Gallery in Tokyo. He directed art reviews such as L'Œil and Connaissance des Arts and received the Elie Faure Prize in 1981 for his publications. His TASCHEN titles include Salvador Dalí: The Paintings, Matisse, and Erotica Universalis.</p>
+
+
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs centered">
+				<img src="http://placehold.it/300x600"
+			</div>
+		</div>
+	</div>
+</div>
+<?php include('footer.php') ?>
+</body>
+</html>
---
+++ article8.php
@@ -0,0 +1,104 @@
+<html lang="en-US">
+    <?php include('header.php') ?>
+<div id="article-cover">
+	<div class="container">
+		<img class="img-responsive" src="http://placehold.it/1280x480">
+		<div class="row">
+		</div>
+	</div>
+</div>
+<div id="article-content">
+	<div class="container epsilon">
+		<div class="row">
+			<div id="headline" class="col-lg-12">
+				<div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+					<h1 class="gamma">Taschen: Coming Soon</h1>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+				</div>
+			</div>
+			<div class="col-lg-8 col-md-8 col-sm-8">
+				<ol>
+<li>100 Contemporary Architects
+<em>Philip Jodidio</em>
+<h4>Keeping up with the fast-paced world of architecture</h4>
+
+<h5>The indispensable compendium of the most cutting-edge architects around</h5>
+
+<p>This two-volume compilation brings together highlights from TASCHEN’s groundbreaking title Building a New Millennium and the first four volumes of our renowned Architecture Now! books for a comprehensive study of architecture around the world at the dawn of the 21st century. Up-and-coming architects—tomorrow’s superstars—are featured alongside the field’s most respected practitioners, such as <em>* Santiago Calatrava, Herzog &amp; de Meuron, Koolhaas/OMA, Daniel Libeskind, Richard Meier, Oscar Niemeyer, and Jean Nouvel.</em>*</p>
+
+<h6>The author:</h6>
+
+<p>Philip Jodidio (born 1954) studied art history and economics at Harvard, and edited Connaissance des Arts for over 20 years. His books include TASCHEN’s Architecture Now! series, and monographs on Tadao Ando, Norman Foster, Richard Meier, Jean Nouvel, and Zaha Hadid. He is internationally renowned as one of the most popular writers on the subject of architecture.</p></li>
+
+<li>100 Illustrators
+<em>Steven Heller, Julius Wiedemann</em>
+
+<h4>The biggest players in the highly competitive field of illustration</h4>
+
+<h5>Culled from our Illustration Now! series is this selection of today’s most successful and important illustrators from around the globe. In his introduction, Steven Heller fleshes out the challenging process of narrowing down a field of 600 illustrators to a selection of the 100 most significant. The final cut, including artists such as Istvan Banyai, Gary Baseman, Seymour Chwast, Paul Davis, Brad Holland, Mirko Ilić, Anita Kunz, and Christoph Niemann, provides a snapshot of the highly dynamic and diverse world of contemporary illustration.</h5>
+
+<p><strong>Features include:</strong>
+* A self-portrait by each illustrator and examples of their work
+* A profile text about each illustrator by Steven Heller
+* Lists of selected exhibitions and publications
+* Illustrators’ quotes about his/her work</p>
+
+<h6>The editors:</h6>
+
+<p><strong>Steven Heller</strong> is the co-chair of the School of Visual Arts MFA Designer as Author Program. For 33 years he was an art director for The New York Times, and currently writes the "Visuals" column for The New York Times Book Review. He is the author of 120 books on graphic design, illustration, and satiric art.
+<strong>Julius Wiedemann</strong> was born in Brazil, studied graphic design and marketing, and was an art editor for digital and design magazines in Tokyo. His many TASCHEN digital and media titles include Illustration Now!, Advertising Now, Logo Design, and Brand Identity Now!</p></li>
+
+<li>Erotica Universalis  Gilles Néret
+
+<h4>The art of pleasure</h4>
+
+<h5>An erotic history of humanity</h5>
+
+<p>From the dawn of time, ever since Adam and Eve, all artists of every age—whether the Egyptian, Greek, or Roman artists of Antiquity, or more recent famous names as Rembrandt, Courbet, Degas, or Picasso—have succumbed to their fantasies, obsessions, and libido, and produced erotic works that the censors have taken good care to keep from the public. For Erotica Universalis, we surface from the subterranean realms of the museums to enter those of our national and private libraries. Here we discover that not only most of our famous writers, such as Ovid, Aretino, Voltaire, Verlaine, or Maupassant, wrote erotic texts that bordered on indecency, but also that great artists like Boucher, Fragonard, Dalí, or Matisse were inspired to provide suitable illustrations for these naughty books. For this new hardcover edition of the classic 1995 bestseller, we have culled highlights from our Erotica Universalis collection. A banquet for the imagination and a feast of the eye! </p>
+
+<h6>The author:</h6>
+
+<p>Gilles Néret (1933–2005) was an art historian, journalist, writer and museum correspondent. He organized several art retrospectives in Japan and founded the SEIBU museum and the Wildenstein Gallery in Tokyo. He directed art reviews such as L'Œil and Connaissance des Arts and received the Elie Faure Prize in 1981 for his publications. His TASCHEN titles include Salvador Dalí: The Paintings, Matisse, and Erotica Universalis.</p></li>
+
+<li>1000 Lights Charlotte &amp; Peter Fiell
+
+<h4>Light up your life</h4>
+
+<h5>Luminary design of the 20th century</h5>
+
+<p>Designed to be a companion to our classic title 1000 Chairs, this edition contains an awesome selection of over 1,000 lights. Presented chronologically by decade are the 20th century’s most interesting electric lights, from Tiffany’s beautiful leaded-glass shades to completely outrageous designs from the late 1960s and 1970s to the latest high-tech LED lamps. All major styles are represented here—Arts &amp; Crafts, Art Nouveau, Art Deco, Modern Movement, De Stijl, Postwar, Pop, Radical, Post-Modern, and Contemporary—in 640 pages of truly illuminated works.</p>
+
+<p>This definitive reference work is a must-have for collectors and design fans.</p>
+
+<h6>The editor and author:</h6>
+
+<p>Charlotte &amp; Peter Fiell have written numerous TASCHEN books, including 1000 Chairs, Design of the 20th Century, Industrial Design A–Z, Scandinavian Design, Designing the 21st Century, Graphic Design for the 21st Century, 1000 Lights and Contemporary Graphic Design. They also edited TASCHEN’s Decorative Art series and the 12-volume domus 1928–1999.</p>
+
+<p>5.- Green Architecture Now! Vol. 1 Philip Jodidio</p>
+
+<h4>How green is your blueprint?</h4>
+
+<h5>Architecture as sustainable technology</h5>
+
+<p>The ecological impact of new construction, once a secondary concern, has become a crucial issue in recent years. Badly designed buildings guzzle natural resources and pollute their surroundings. In an era of rocketing energy costs and environmental degradation, the need for a sustainable, energy-efficient architecture is paramount. This book examines the emergent innovations, aesthetics, and pioneers of green architecture, including such artists, architects and firms as Tadao Ando, Shigeru Ban, Diller Scofidio + Renfro, Zaha Hadid, Morphosis, and many more. </p>
+
+<h6>The author:</h6>
+
+<p>Philip Jodidio (born 1954) studied art history and economics at Harvard, and edited Connaissance des Arts for over 20 years. His books include TASCHEN’s Architecture Now! series, and monographs on Tadao Ando, Norman Foster, Richard Meier, Jean Nouvel, and Zaha Hadid. He is internationally renowned as one of the most popular writers on the subject of architecture.</p>
+
+<p>[www.taschen.com] (http://www.taschen.com)</p>
+</li></ol>
+
+
+
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 hidden-xs centered">
+				<img src="http://placehold.it/300x600"
+			</div>
+		</div>
+	</div>
+</div>
+<?php include('footer.php') ?>
+</body>
+</html>
